2ca221e3686ea8eaa763be331c9eace80ac8dce0
krenol <lauritzgroeger@gmail.com>
2020-08-03 15:31:59 +0200

    small change to func

--- utils/src/env.hpp
+++ utils/src/env.hpp
@@ -8,7 +8,7 @@ namespace utils {
     class Env {
         public:
             static std::string getEnvVar(const std::string& varName, const std::string& defaultValue = "");
-            static void getEnvVar(const std::string& varName, std::string& outValue, const std::string& defaultValue = "");
+            static void getEnvVar(const std::string& varName, std::string& outValue, const std::string& defaultValue);
     };
 }
 
